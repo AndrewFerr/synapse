f69ddc174e9ab4131e6693d31e97907ee8c3ce8e
Andrew Ferrazzutti <andrewf@element.io>
2026-03-16 13:58:07 -0400

    Update copyright dates

--- tests/config/test_server.py
+++ tests/config/test_server.py
@@ -1,7 +1,8 @@
 #
 # This file is licensed under the Affero General Public License (AGPL) version 3.
 #
-# Copyright (C) 2023 New Vector, Ltd
+# Copyright (C) 2023-2025 New Vector Ltd
+# Copyright (C) 2025-2026 Element Creations Ltd
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
